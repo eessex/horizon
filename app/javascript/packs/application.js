c4859ff64db28b3df37fb60db2217d288dd39612
Eve Essex <eve.essex@gmail.com>
2020-06-02 16:51:23 -0400

    migrate asset pipeline for webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,8 @@ import "regenerator-runtime/runtime";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
